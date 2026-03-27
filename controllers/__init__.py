4a6a970796bdab80170cd0ea38ef3e2dc03b6ce7
AdrielBernalS <bersadriel@gmail.com>
2026-03-27 00:59:55 -0500

    Hora PERU

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -5900,7 +5900,7 @@ def init_reportes_controller(app):
                     plan_info = cursor.fetchone()
                     permite_aplazamiento = plan_info['permite_aplazamiento'] == 1 if plan_info else False
                     # Clientes de un plan específico - CON MÉTODO Y ESTADO CORREGIDOS
-                    cursor.execute('''
+                    cursor.execute(f'''
                         SELECT 
                             c.id,
                             c.nombre_completo as nombre,
